44994aa82a25cc3f3e2f62b87ed94dd214fb4fbf
Sourabh Munjal <somunjal@homeaway.com>
2020-12-30 15:16:43 +0530

    fixed icons and first card on carousel hover

--- src/app/components/similar/similar.component.ts
+++ src/app/components/similar/similar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as Similar from '../../talkies/store/similar/similar.reducer';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { tns } from "tiny-slider/src/tiny-slider"
 
 @Component({
   selector: 'app-similar',
@@ -11,20 +12,38 @@ import { Router } from '@angular/router';
 })
 export class SimilarComponent implements OnInit {
   similarMovies: Object
+  carouselInitiated: boolean = false;
   constructor(
     private store: Store<Similar.SimilarState>,
     private router: Router,
     private sanitizer: DomSanitizer
-  ) {
-    this.store.select('similar').subscribe((data) => {
-      console.log('similar movies ', data);
-      if (data) {
-        this.similarMovies = data.similarMovies;
-      }
-    });
-  }
+    ) {
+      this.store.select('similar').subscribe((data) => {
+        console.log('similar movies ', data);
+        if (data) {
+          this.similarMovies = data.similarMovies;
+        }
+      });
+    }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    if (!this.carouselInitiated) {
+
+      setTimeout(() => {
+        var slider = tns({
+          container: '.my-slider',
+          // items: 3,
+          slideBy: 'page',
+          fixedWidth: 200,
+          mouseDrag: true,
+          controls: false,
+        });
+        this.carouselInitiated = true;
+      }, 2000)
+    }
   }
 
   routeToDetail(movie) {
@@ -33,5 +52,9 @@ export class SimilarComponent implements OnInit {
   getBackground(posterPath) {
     return this.sanitizer.bypassSecurityTrustResourceUrl('https://image.tmdb.org/t/p/w220_and_h330_face' + posterPath);
 
+  }
+
+  isMobile(): boolean {
+    return window.matchMedia("only screen and (max-width: 760px)").matches;
   }
 }
